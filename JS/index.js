59e3cf86bc4e80d3f81adfdd5ab5d7774aa2532a
inigo <ladrongue5@gmail.com>
2022-11-03 13:54:23 +0100

    exa1

---
+++ JS/index.js
@@ -0,0 +1,32 @@
+"use strict"
+document.getElementById("bEnviar").addEventListener("click", validar);
+document.getElementById("bBorrar"), addEventListener("click", borrar);
+
+function validar() {
+    try {
+        let exVal = new RegExp(/^[A-Z]{1}[a-z]+$/);
+
+        let nombre = document.getElementById("nombre").value;
+        let apellido = document.getElementById("apellido").value;
+        let correo = document.getElementById("correo").value;
+        let provincia = document.getElementById("provincia").value;
+
+
+    } catch (error) {
+        alert(error)
+    }
+}
+
+function borrar() {
+    alert(1)
+    try {
+        document.getElementById("nombre").value = "";
+        document.getElementById("apellido").value = "";
+        document.getElementById("correo").value = "";
+        document.getElementById("provincia").value = "";
+
+
+    } catch (error) {
+        alert("Error al borrar")
+    }
+}
