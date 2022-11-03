6f5dd4236c6b2a2dc0b620a4013a77e19cdd614e
inigo <ladrongue5@gmail.com>
2022-11-03 18:05:11 +0100

    jsCambios

--- JS/index.js
+++ JS/index.js
@@ -6,15 +6,35 @@ function validar() {
     try {
 
         let exVal = new RegExp(/^[A-Z]{1}[a-z]+$/);
+        let exgm = new RegExp('^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$');
 
         let nombre = document.getElementById("nombre").value;
         let apellido = document.getElementById("apellido").value;
         let correo = document.getElementById("correo").value;
         let provincia = document.getElementById("provincia").value;
+
         if (!exVal.test(nombre)) {
             document.getElementById("nombre").focus();
-
+            throw "En el formulario hay datos MAL";
         }
+
+        if (!exVal.test(apellido)) {
+            document.getElementById("apellido").focus();
+            throw "En el formulario hay datos MAL";
+        }
+
+        if (!exgm.test(correo)) {
+            document.getElementById("correo").focus();
+            throw  "En el formulario hay datos MAL"
+        }
+        
+        if (exVal.test("provincia")) {
+            document.getElementById("provincia").focus();
+            throw  "En el formulario hay datos MAL"
+            
+        }
+
+        
 
     } catch (error) {
         alert(error)
