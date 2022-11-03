1f7cc3d5f8a18e381c1563101d3795873ec141aa
inigo <ladrongue5@gmail.com>
2022-11-03 14:09:17 +0100

    ejer01

--- JS/index.js
+++ JS/index.js
@@ -4,13 +4,17 @@ document.getElementById("bBorrar").addEventListener("click", borrar);
 
 function validar() {
     try {
+
         let exVal = new RegExp(/^[A-Z]{1}[a-z]+$/);
 
         let nombre = document.getElementById("nombre").value;
         let apellido = document.getElementById("apellido").value;
         let correo = document.getElementById("correo").value;
         let provincia = document.getElementById("provincia").value;
+        if (!exVal.test(nombre)) {
+            document.getElementById("nombre").focus();
 
+        }
 
     } catch (error) {
         alert(error)
